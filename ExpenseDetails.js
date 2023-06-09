52eccbe2bfdeae2dd893245bc407625b4103f08c
Gobinath7494 <134025947+Gobinath7494@users.noreply.github.com>
2023-06-09 13:17:56 +0530

    creating new component - ExpenseDate

---
+++ ExpenseDetails.js
@@ -0,0 +1,17 @@
+import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
+
+function ExpenseDetails(props) {
+  return (
+    <div className="expense-item">
+      <ExpenseDate date={props.date} />
+      <ExpenseDetails
+        amount={props.amount}
+        location={props.location}
+        title={props.title}
+      />
+    </div>
+  );
+}
+
+export default ExpenseDetails;
--- ExpenseItem.js
+++ ExpenseItem.js
@@ -1,9 +1,14 @@
+import ExpenseDate from "./ExpenseDate";
+// import ExpenseDetails from "./ExpenseDetails";
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
   return (
     <div className="expense-item">
-      <div>{props.date.toISOString()}</div>
+      <ExpenseDate date={props.date}></ExpenseDate>
+      {/* <ExpenseDetails
+        LocationOfExpenditure={props.LocationOfExpenditure}
+      ></ExpenseDetails> */}
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <h2>{props.LocationOfExpenditure}</h2>
